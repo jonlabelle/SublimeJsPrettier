161d725606ad074974a76cdc8c6325ae7528bdd7
Jon LaBelle <contact@jonlabelle.com>
2017-08-31 08:20:16 -0500

    Allow dotenv vars

--- scripts/run_sonar_scan.sh
+++ scripts/run_sonar_scan.sh
@@ -7,13 +7,20 @@ SCRIPTSDIR="$(cd "$(dirname "${0}")"; echo "$(pwd)")"
 SONAR_LOGIN=$1
 SONAR_SCANNER_CMD=$2
 
+pushd "${SCRIPTSDIR}" && pushd ..
+
+# if no args passed... try to suck suck .env file:
 if [ $# -eq 0 ]; then
-    echo "Usage: run_sonar_scan.sh <sonar login/api key> [sonar scanner command path]"
-    exit 1
+    if [ -r .env ] && [ -f .env ]; then
+        while read -r line; do export $line; done < .env
+    else
+        echo "Usage: run_sonar_scan.sh <sonar login/api key> [sonar scanner command path]"
+        exit 1
+    fi
 fi
 
 if [ -z "$SONAR_LOGIN" ]; then
-    echo "ERROR: Missing positional arg(1) for 'sonar.login' (api token)"
+    echo "ERROR: Missing positional arg(1) for 'SONAR_LOGIN' (api token)"
     exit 1
 fi
 if [ -z "$SONAR_SCANNER_CMD" ]; then
@@ -24,7 +31,6 @@ echo
 echo '> Run sonar scanner analysis'
 echo
 
-pushd "${SCRIPTSDIR}" && pushd ..
 "${SONAR_SCANNER_CMD}" -Dsonar.login="${SONAR_LOGIN}"
 popd && popd
 
