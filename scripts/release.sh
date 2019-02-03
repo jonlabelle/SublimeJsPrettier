9233ec5751bb99f2838a182ad457adc0ba213a4f
Jon LaBelle <contact@jonlabelle.com>
2019-02-03 13:33:50 -0600

    Update release script (bump to major/minor/patch)

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,36 +1,11 @@
 #!/usr/bin/env bash
 
-##
-# Release tasks bulked into one script w/ user intervention/confirmation
-# required.
-#
-# - Bumps 'package.json' version field to the passed <semver> arg (1).
-# - Commit and push the change (to master).
-# - Create and push passed <semver> arg (1) git tag.
-# - Run npm publish.
-#
-# Usage:
-#
-#     release.sh [options] <version>
-#
-# Examples:
-#
-#   To bump and tag to the specified version (1.2.1):
-#
-#     release.sh 1.2.1
-#
-#   To bump and tag automatically incrementing to the next patch version:
-#
-#     release.sh --next
-##
-
 set -e
 set -o pipefail
 [ "$TRAVIS" == "true" ] && set -x
 
-VERSION=$1
-PREVIOUS_VERSION="$(git describe --abbrev=0 --tags)"
-NEXT_VERSION="${PREVIOUS_VERSION%.*}.$((${PREVIOUS_VERSION##*.}+1))"
+NEW_VERSION=
+CURRENT_VERSION="$(git describe --abbrev=0 --tags)"
 
 readonly PREVIOUSWRKDIR="$(pwd)"
 readonly SCRIPTSDIR="$(cd "$(dirname "${0}")"; echo "$(pwd)")"
@@ -62,26 +37,81 @@ show_error() {
 }
 
 show_usage() {
-    echo "Release(.sh) tasks simplified."
+    echo "release(.sh) tasks simplified."
     echo
-    echo "Usage: $SCRIPTNAME [options] <semver>"
+    echo "Usage: $SCRIPTNAME [options] <major|minor|patch|semver>"
+    echo
+    echo "Positional Arguments:"
+    echo
+    echo "  patch    auto-increment to the next patch version."
+    echo "  minor    auto-increment to the next minor version."
+    echo "  major    auto-increment to the next major version."
+    echo "  semver   the specific version number (as semver) for the release."
     echo
     echo "Options:"
     echo
-    echo "    -n, --next    auto-increment to the next patch version"
-    echo "    -h, --help    show usage"
+    echo "  -h, --help     show help/usage, and exit."
+    echo "  -v, --version  show the current version, and exit."
     echo
-    echo "Positional Arguments:"
-    echo 
-    echo "    <semver>      the semver/version number for the release"
+    echo "Examples:"
+    echo
+    echo "Bump to the next 'major' version:"
+    echo
+    echo "  $SCRIPTNAME major"
+    echo
+    echo "Bump to the next 'minor' version:"
+    echo
+    echo "  $SCRIPTNAME minor"
+    echo
+    echo "Bump to the next 'patch' version:"
+    echo
+    echo "  $SCRIPTNAME patch"
+    echo
+    echo "Bump to the specified version:"
+    echo
+    echo "  $SCRIPTNAME 1.2.1"
     echo
 }
 
+bump_new_version() {
+    local semver_part_to_bump oldIFS version_parts major minor patch
+
+    semver_part_to_bump=$1
+
+    oldIFS="$IFS"
+    IFS='.' read -a version_parts <<< "$CURRENT_VERSION"
+    IFS="$oldIFS"
+
+    major=${version_parts[0]}
+    minor=${version_parts[1]}
+    patch=${version_parts[2]}
+
+    case "$semver_part_to_bump" in
+        "major")
+            major=$((major + 1))
+            minor=0
+            patch=0
+            ;;
+        "minor")
+            minor=$((minor + 1))
+            patch=0
+            ;;
+        "patch")
+            patch=$((patch + 1))
+            ;;
+    esac
+
+    NEW_VERSION="$major.$minor.$patch"
+}
+
 validate_version() {
-    if [[ ! ${VERSION} =~ ^[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
-        show_error "'$VERSION' is not a valid semver/version number (ex: 1.2.1)."
-        exit 1
+    local _version
+    _version=$1
+    if [[ ! ${_version} =~ ^[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
+        show_error "'${_version}' is not a valid semver/version number (ex: 1.2.1)."
+        return 1
     fi
+    return 0
 }
 
 ensure_git_branch_is_master() {
@@ -113,10 +143,10 @@ ensure_git_repo_is_clean() {
 }
 
 bump_package_json_version() {
-    show_info "> Bump version in 'package.json' file, 'v$PREVIOUS_VERSION' -> 'v$VERSION'"
+    show_info "> Bump version in 'package.json' file, 'v$CURRENT_VERSION' -> 'v$NEW_VERSION'"
     local tmp_pkg_file="${TMPDIR:-/tmp}/package.json.$$"
-    sed -E s/'"version"\: "[0-9]+\.[0-9]+\.[0-9]+"'/'"version"\: "'"$VERSION"'"'/ package.json > "$tmp_pkg_file" && mv "$tmp_pkg_file" package.json
-    grep "$VERSION" -C 1 package.json
+    sed -E s/'"version"\: "[0-9]+\.[0-9]+\.[0-9]+"'/'"version"\: "'"$NEW_VERSION"'"'/ package.json > "$tmp_pkg_file" && mv "$tmp_pkg_file" package.json
+    grep "$NEW_VERSION" -C 1 package.json
 }
 
 ensure_only_one_file_changed() {
@@ -130,7 +160,7 @@ ensure_only_one_file_changed() {
 confirm_git_commit_tag_release() {
     show_info "> Acquire confirmation"
     while true; do
-        read -r -p "Ready to commit, tag and release 'v$VERSION'? (y/n): " yn
+        read -r -p "Ready to commit, tag and release 'v$NEW_VERSION'? (y/n): " yn
         case ${yn} in
             [Yy]* )   break;;
             [NnQq]* ) exit;;
@@ -143,9 +173,9 @@ git_commit_tag_release() {
     show_info "> git commit/push/tag/push --tags"
     set -x
     git add package.json
-    git commit -m "Bump to v$VERSION"
+    git commit -m "Bump to v$NEW_VERSION"
     git push origin master
-    git tag -a "$VERSION" -m "Tag v$VERSION"
+    git tag -a "$NEW_VERSION" -m "Tag v$NEW_VERSION"
     git push --tags
     set +x
 }
@@ -157,7 +187,6 @@ npm_publish() {
 
 
 main() {
-    validate_version
     cd_project_root
     ensure_git_branch_is_master
     ensure_git_branch_is_up_to_date
@@ -174,16 +203,31 @@ main() {
 
 if [ $# -eq 0 ]; then
     show_error "a valid semver/version number for the new release is required"
-    echo "Currently, the latest release is '$(show_success "${PREVIOUS_VERSION}")'"
+    echo
+    echo "Currently, the latest release is '$(show_success "${CURRENT_VERSION}")'"
+    echo
     show_usage
     exit 1
 else
-    if [ "$1" = "-n" ] || [ "$1" = "--next" ]; then
-        VERSION=${NEXT_VERSION}
-    fi
-    if [ "$1" = "-h" ] || [ "$1" = "--help" ]; then
+    if [ "$1" = "patch" ] || [ "$1" = "minor" ] || [ "$1" = "major" ]; then
+        bump_new_version "$1"
+        main
+    elif [ "$1" = "-v" ] || [ "$1" = "--version" ]; then
+        echo "${CURRENT_VERSION}"
+        exit 0
+    elif [ "$1" = "-h" ] || [ "$1" = "--help" ]; then
         show_usage
         exit 0
+    else
+        # check if the first arg is a valid semver... if so, use it.
+        validate_version $1
+        if [ $? == 0 ]; then
+            NEW_VERSION=$1
+            main
+        else
+            show_error "no valid arguments supplied."
+            show_usage
+            exit 1
+        fi
     fi
-    main
 fi
