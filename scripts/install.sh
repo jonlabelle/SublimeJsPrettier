f7bc638fd4cacd9b072cf9970752d1d340ea3ddc
Jon LaBelle <contact@jonlabelle.com>
2024-04-05 19:14:46 -0500

    Don't upgrade pip
    ... because of local error, 'error:externally-managed-environment'
    
    Need to use venv.

--- scripts/install.sh
+++ scripts/install.sh
@@ -9,9 +9,14 @@ readonly SCRIPTSDIR="$(cd "$(dirname "${0}")"; echo "$(pwd)")"
 cd_project_root() { cd "${SCRIPTSDIR}" && cd ..; }
 
 install_pip_requirements() {
+    echo && echo '> Activate python virtual environment (venv)'
+    source venv/bin/activate
+
+    echo && echo '> Ensure pip upgrade'
+    python -m ensurepip --upgrade
+
     echo && echo '> Install pip requirements'
-    #python3 -m pip install --upgrade pip
-    pip install -r requirements.txt
+    python -m pip install -r requirements.txt
 }
 
 install_npm_packages() {
@@ -23,7 +28,6 @@ main() {
     cd_project_root
     install_pip_requirements
     install_npm_packages
-
     echo && echo 'Finished.'
 }
 
