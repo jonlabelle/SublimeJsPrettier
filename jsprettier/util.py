9b6eb11048f8038f86f5d3ba4d048ec0223caa07
Jon LaBelle <contact@jonlabelle.com>
2024-05-02 08:23:45 -0500

    Minor verbiage updates

--- jsprettier/util.py
+++ jsprettier/util.py
@@ -407,9 +407,10 @@ def decode_bytes(bytes_to_decode):
 def wrap_in_double_quotes(val):
     """
     Fixes a corner case when attempting to format a file on
-    Windows and default shell is fish.
+    Windows and default shell is fish, and a path fragment
+    is wrapped in parentheses.
 
-    NOTE: Only use with '--stdin-filepath', otherwise
+    WARNING: Only use with '--stdin-filepath', otherwise
     breaks command when using with other path options (e.g. '--config').
 
     See #285: https://github.com/jonlabelle/SublimeJsPrettier/issues/285
