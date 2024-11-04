6cfd7de8f7d96d70582823cd71c824842b675781
Jon LaBelle <contact@jonlabelle.com>
2024-11-04 13:55:33 -0600

    refactor: remove hard-coded cmd shell

--- JsPrettier.py
+++ JsPrettier.py
@@ -422,8 +422,8 @@ class JsPrettierCommand(sublime_plugin.TextCommand):
                 stderr=PIPE,
                 stdout=PIPE,
                 env=get_proc_env(),
-                shell=is_windows(),
-                executable='C:\\Windows\\System32\\cmd.exe' if is_windows() else None)
+                shell=is_windows()
+            )
 
             stdout, stderr = proc.communicate(input=source.encode('utf-8'))
             if proc.returncode != 0:
