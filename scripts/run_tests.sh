9f96ff8d7edaff3b1333d7a016c21ec7435aa5f5
Jon LaBelle <contact@jonlabelle.com>
2024-11-05 17:55:03 -0600

    fix(test): ensure venv is activated

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -8,31 +8,52 @@ readonly SCRIPTSDIR="$(cd "$(dirname "${0}")"; echo "$(pwd)")"
 
 cd_project_root() { cd "${SCRIPTSDIR}" && cd ..; }
 
+activate_venv() {
+    if [[ -d "venv" ]]; then
+        # shellcheck disable=SC1091
+        source venv/bin/activate
+    fi
+}
+
 run_pytest() {
     echo
     echo '> Run pytest'
-    python -m pytest
+    activate_venv
+    python -m pytest || {
+        echo "pytest failed"
+        exit 1
+    }
 }
 
 run_flake8() {
     echo
     echo '> Run flake8'
+    activate_venv
     echo -n 'Total errors: '
-    flake8 . --count --show-source --statistics
+    python -m flake8 . --count --show-source --statistics || {
+        echo "flake8 failed"
+        exit 1
+    }
 }
 
 run_pylint() {
     echo
     echo '> Run pylint'
-    pylint .
+    activate_venv
+    python -m pylint . || {
+        echo "pylint failed"
+        exit 1
+    }
 }
 
 run_markdownlint() {
     echo '> Run markdownlint'
-    markdownlint-cli2 .
+    markdownlint-cli2 . || {
+        echo "markdownlint failed"
+        exit 1
+    }
     echo 'Markdown looks good.' && echo
 }
-
 
 main() {
     cd_project_root
